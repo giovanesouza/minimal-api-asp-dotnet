feb7e619edbfc9f525e66ae9a3aa4f035ed28a6b
Giovane Souza <developergiovanesouza@gmail.com>
2025-10-26 22:10:18 -0300

    feat(db): Db configuration and migration

--- Api/Infrastructure/Db/DbContext.cs
+++ Api/Infrastructure/Db/DbContext.cs
@@ -1,6 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.Entities;
 namespace MinimalApi.Infrastructure.Db;
 
-public class DbContext
+public class DBContext : DbContext
 {
+    private readonly IConfiguration _configurationAppSettings;
+    public DBContext(IConfiguration configurationAppSettings)
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
 
+    public DbSet<Administrator> Administrators { get; set; } = default!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+            if (connectionString != null)
+            {
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString)
+                );
+                return;
+            }
+        }
+    }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,22 @@
+using MinimalApi.Infrastructure.Db;
+using MinimalApi.Domain.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DBContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.Domain.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
     if (loginDTO.Email == "adm@test.com" && loginDTO.Password == "123456")
     {
         return Results.Ok("Login realizado com sucesso!");
