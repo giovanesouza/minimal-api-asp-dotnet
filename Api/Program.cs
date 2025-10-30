bae91ff578d48f014bf6251aeebd507151106010
Giovane Souza <developergiovanesouza@gmail.com>
2025-10-29 23:40:08 -0300

    doc: config swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,10 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using MinimalApi.Domain.Interfaces;
 using MinimalApi.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Domain.ModelViews;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministratorService, AdministratorService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DBContext>(options =>
 {
@@ -19,7 +23,7 @@ builder.Services.AddDbContext<DBContext>(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) => {
     if (administratorService.Login(loginDTO) != null)
@@ -29,5 +33,7 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService admin
     return Results.Unauthorized();
 });
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
