e155b9971e44252fa0aba71f1008a65dc8675669
Giovane Souza <developergiovanesouza@gmail.com>
2025-10-31 00:37:31 -0300

    feat: create vehicle

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,14 +1,17 @@
-using MinimalApi.Infrastructure.Db;
-using MinimalApi.Domain.DTOs;
-using Microsoft.EntityFrameworkCore;
-using MinimalApi.Domain.Interfaces;
-using MinimalApi.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.DTOs;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
 using MinimalApi.Domain.ModelViews;
+using MinimalApi.Domain.Services;
+using MinimalApi.Infrastructure.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministratorService, AdministratorService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,18 +25,38 @@ builder.Services.AddDbContext<DBContext>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) => {
+#region Administrators
+app.MapPost("/administrators/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) => {
     if (administratorService.Login(loginDTO) != null)
     {
         return Results.Ok("Login realizado com sucesso!");
     }
     return Results.Unauthorized();
 });
+#endregion
 
+#region Vehicles
+app.MapPost("/vehicles", ([FromBody] VehicleDTO VehicleDTO, IVehicleService vehicleService) => {
+    var vehicle = new Vehicle
+    {
+        Name = VehicleDTO.Name,
+        Brand = VehicleDTO.Brand,
+        Year = VehicleDTO.Year
+    };
+    var createdVehicle = vehicleService.Create(vehicle);
+    return Results.Created($"/vehicle/{createdVehicle.Id}", createdVehicle);
+});
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
